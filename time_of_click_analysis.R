faaa56aea5258c703c924b9e232ac548fdddca11
Vicky Banks <victoria.banks052@gmail.com>
2020-01-29 12:10:17 +0000

    Added functionality to view clicks by which menu they were on.

--- time_of_click_analysis.R
+++ time_of_click_analysis.R
@@ -5,19 +5,21 @@ library(ggrepel)
 
 clickTime<- read.csv("time_to_click.csv", header = TRUE)
 clickTime <- clickTime %>% rename(clickTime_sec = time_since_content_start_sec)
-#clickTime$visit_id <- as.factor(clickTime$visit_id)
+clickTime <- clickTime %>% rename(menuType = menu_type)
 clickTime<- na.omit(clickTime)
 clickTime %>% filter(is.na(clickTime_sec))
 
 clickTime %>% filter(clickTime_sec<0)
 summary(clickTime)
 
+## Look at data in groups of first 1 minute, first 5, etc
 clickTimeGroups<- clickTime %>% 
   mutate(timeRange_sec = cut(clickTime_sec, 
                              breaks = c(-1, 60, 300, 600, 900, 1200, Inf),
                              labels = c("0-1", "1-5", "5-10", "10-15", "15-20", "20+"))) %>% 
   group_by(timeRange_sec) %>% 
   summarize(numInRange=n())
+
 
 ggplot(data = clickTimeGroups, aes(x=timeRange_sec, y=numInRange))+
   geom_bar(stat = "identity")+
@@ -26,7 +28,7 @@ ggplot(data = clickTimeGroups, aes(x=timeRange_sec, y=numInRange))+
   xlab("Time Range (mins)")
   theme_classic()
 
- ############## 
+ ### Look at data by minute
   clickTimeMinutes<- clickTime %>% 
     mutate(timeRange_sec = cut(clickTime_sec, 
                                breaks = c(-1, 60,120,180,240, 300,360,420,480,540, 600,Inf),
@@ -41,7 +43,7 @@ ggplot(data = clickTimeGroups, aes(x=timeRange_sec, y=numInRange))+
     ylab("Number of Visits with Click in Time Range (millions)")+
     xlab("Time Range (mins)")+
     geom_hline(yintercept = 250000)+
-    geom_label(data=subset(clickTimeMinutes, perc > 5),
+    geom_label(data=subset(clickTimeMinutes, perc > 6),
               aes(label=paste0(sprintf("%1.0f", perc),"%"),),
               position = position_stack(vjust = 0.5),
               colour="black")+
@@ -50,5 +52,37 @@ ggplot(data = clickTimeGroups, aes(x=timeRange_sec, y=numInRange))+
                position = position_stack(vjust = 1.6),
                colour="black")+
     ggtitle("Number of Clicks to the Onward Journey Panel 'x' Minutes After Content Start \n PS_IPLAYER - Big Screen - 2019-11-01 to 2019-11-14" )+
-  theme_classic()
+  theme_classic() 
 
+  
+## Look at data depending on which menu the click was from 
+  clickTimeMinsMenu<- clickTime %>% 
+    mutate(timeRange_sec = cut(clickTime_sec, 
+                               breaks = c(-1, 60,120,180,240, 300,360,420,480,540, 600,Inf),
+                               labels = c("0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-7", "7-8", "8-9", "9-10", "10+"))) %>% 
+    group_by(menuType, timeRange_sec) %>% 
+    summarize(numInRange=n()) %>%
+    mutate(perc = round(100*numInRange/sum(numInRange),1)) 
+  
+  
+  ggplot(data = clickTimeMinsMenu, aes(x=timeRange_sec, y=numInRange/1000000, fill = menuType))+
+    geom_bar(stat = "identity")+
+    #scale_y_continuous(limits = c(0,5000000), breaks = c(0,250000, 1000000,2000000,3000000,4000000,5000000), labels = c(0,0.25, 1,2,3,4,5))+
+    ylab("Number of Visits with Click in Time Range (millions)")+
+    xlab("Time Range (mins)")+
+    #geom_hline(yintercept = 250000)+
+    geom_label(data=subset(clickTimeMinsMenu, perc >= 10),
+               aes(label=paste0(sprintf("%1.0f", perc),"%"),),
+               position = position_stack(vjust = 0.5),
+               colour="black",
+               fill = "white")+
+    geom_label(data=subset(clickTimeMinsMenu, perc >= 5 & perc < 10),
+               aes(label=paste0(sprintf("%1.0f", perc),"%"),),
+               position = position_stack(vjust = 1.6),
+               colour="black", 
+               fill = "white")+
+    ggtitle("Number of Clicks to the Onward Journey Panel 'x' Minutes After Content Start \n PS_IPLAYER - Big Screen - 2019-11-01 to 2019-11-14" )+
+    theme_classic() +
+    facet_wrap(~ menuType, nrow = 1, scales = "free")+
+    theme(legend.position = "none")
+  
